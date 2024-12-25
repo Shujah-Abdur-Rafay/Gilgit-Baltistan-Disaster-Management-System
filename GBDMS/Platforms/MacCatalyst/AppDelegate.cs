223ec8180b6be17a5fcf477debf94f8ea85c8c81
Shujah <shujaharafay7@gmail.com>
2024-12-26 01:41:40 +0500

    Add project files.

---
+++ GBDMS/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GBDMS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
